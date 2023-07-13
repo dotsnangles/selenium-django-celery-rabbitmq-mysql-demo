95ec61d5f9aeb66998f878aaa3f617e6d1338a60
Jeonghyeon Park <dotsnangles@gmail.com>
2023-07-13 20:21:49 +0900

    ADD: charset for db connection

--- manager/settings.py
+++ manager/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         "PASSWORD": "jake",
         "HOST": "127.0.0.1",
         "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     },
     "news_scraper": {
         "ENGINE": "django.db.backends.mysql",
@@ -96,6 +97,7 @@ DATABASES = {
         "PASSWORD": "jake",
         "HOST": "127.0.0.1",
         "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     },
 }
 
